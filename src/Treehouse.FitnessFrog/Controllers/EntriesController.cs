697d25929c8cb915dd3713e3ff579100097213e7
Xiao Peng <xiao.peng1006@gmail.com>
2018-03-27 12:39:22 -0700

    Added AddPost Action Method

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -44,6 +44,13 @@ namespace Treehouse.FitnessFrog.Controllers
             return View();
         }
 
+        [ActionName("Add"),HttpPost]
+        public ActionResult AddPost()
+        {
+            return View();
+        }
+
+
         public ActionResult Edit(int? id)
         {
             if (id == null)
