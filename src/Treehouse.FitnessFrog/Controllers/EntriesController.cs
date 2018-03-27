1b1d9bf646816b5c1841e1a11970216f13dcc726
Xiao Peng <xiao.peng1006@gmail.com>
2018-03-27 12:51:29 -0700

    Updated Add Method to capture POST data

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -45,8 +45,15 @@ namespace Treehouse.FitnessFrog.Controllers
         }
 
         [ActionName("Add"),HttpPost]
-        public ActionResult AddPost()
+        public ActionResult Add(string date, string activityId, string duration, string intensity, string exculde, string notes)
         {
+            ViewBag.Date = date;
+            ViewBag.ActivityId = activityId;
+            ViewBag.Duration = duration;
+            ViewBag.Intensity = intensity;
+            ViewBag.Exclude = exculde;
+            ViewBag.Notes = notes;
+
             return View();
         }
 
